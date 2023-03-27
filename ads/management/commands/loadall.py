1e4540f33fc1018c5ab7190b10839402ba567466
mgd <mr.zizikk@gamil.com>
2023-03-27 09:43:17 -0400

    create custom command loadall, configure app urls, add converter

---
+++ ads/management/commands/loadall.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Loads fixtures from fixtures dir"
+    fixtures_dir = 'fixtures'
+    loaddata_command = 'loaddata'
+    filenames = [
+        "ads.json",
+        "categories.json",
+    ]
+
+    def handle(self, *args, **options):
+        for fixture_filename in self.filenames:
+            if os.path.exists(fixture_filename):
+                call_command(self.loaddata_command, os.path.join(self.fixtures_dir, fixture_filename))
---
+++ converter.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+
+def csv_to_json(csv_path, json_path):
+    data = []
+
+    with open(csv_path, 'r', encoding='UTF-8') as file:
+        cvs_reader = csv.DictReader(file)
+
+        for row in cvs_reader:
+            data.append(row)
+
+    write_json_file(data, json_path)
+
+
+def write_json_file(csv_data, json_path):
+    with open(json_path, 'w', encoding='UTF-8') as file:
+        json_string = json.dumps(csv_data, indent=4)
+        file.write(json_string)
+
+
+if __name__ == '__main__':
+    csv_to_json('./fixtures/ads.csv', './fixtures/ads.json')
+    csv_to_json('./fixtures/categories.csv', './fixtures/categories.json')
